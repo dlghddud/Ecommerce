cb6570269b1140ca2e06ec6833eb39e96a985394
이홍영 <hyhy7012@naver.com>
2026-05-14 02:48:23 +0900

    fix : ReservationService 구조 변경

--- src/main/java/com/example/ecommerce/service/ReservationService.java
+++ src/main/java/com/example/ecommerce/service/ReservationService.java
@@ -2,6 +2,7 @@ package com.example.ecommerce.service;
 
 import com.example.ecommerce.Entity.Reservation;
 import com.example.ecommerce.repository.ReservationRepository;
+import com.example.ecommerce.repository.SeatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -10,18 +11,27 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
+
+    private final SeatRepository seatRepository;
     private final ReservationRepository reservationRepository;
 
     @Transactional
     public String reserve(Long seatId, Long userId) {
 
+        seatRepository.findById(seatId)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("존재하지 않는 좌석"));
+
         try {
+
             reservationRepository.save(
                     new Reservation(seatId, userId)
             );
+
             return "예약 성공";
 
         } catch (DataIntegrityViolationException e) {
+
             return "이미 예약된 좌석입니다.";
         }
     }
